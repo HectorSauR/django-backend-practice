8ee605f263c95e05583f87c70955d9fb689ec845
HectoSauR <hector.sauceda.01@gmail.com>
2024-10-15 15:04:33 -0700

    views and settings
    - views for create, update, list and delete
    - serializers
    - login view as well

--- practicaDjangoBackend/settings.py
+++ practicaDjangoBackend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ('django-insecure-e+f*!m@6@j=&sgrwp!tarm-cdr8xseo0p@!=0%8mg^0-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from user.views import CreateUserAPI, UpdateUserAPI, ListUserAPI, DeleteUserAPI
+
+
+urlpatterns = [
+    path('', ListUserAPI.as_view(), name='list-users'),
+    path('create/', CreateUserAPI.as_view(), name='create-user'),
+    path('<int:pk>/update', UpdateUserAPI.as_view(), name='update-user'),
+    path('<int:pk>/delete', DeleteUserAPI.as_view(), name='delete-user'),
+]
